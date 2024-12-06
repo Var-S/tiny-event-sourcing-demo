6b1b2c6ecf03958d4d086ed9976ffe38e6b64da1
Денис Егошин <egoshin.deniska@mail.ru>
2024-12-06 15:42:45 +0300

    delete useless code in config

--- src/main/kotlin/ru/quipy/config/EventSourcingLibConfiguration.kt
+++ src/main/kotlin/ru/quipy/config/EventSourcingLibConfiguration.kt
@@ -36,24 +36,13 @@ import javax.annotation.PostConstruct
  */
 @Configuration
 class EventSourcingLibConfiguration {
-
-    private val logger = LoggerFactory.getLogger(EventSourcingLibConfiguration::class.java)
-
-    @Autowired
-    private lateinit var subscriptionsManager: AggregateSubscriptionsManager
-
-   // @Autowired
-   // private lateinit var projectEventSubscriber: AnnotationBasedProjectEventsSubscriber
-
     @Autowired
     private lateinit var eventSourcingServiceFactory: EventSourcingServiceFactory
-
-    @Autowired
-    private lateinit var eventStreamManager: AggregateEventStreamManager
 
     /**
      * Use this object to create/update the aggregate
      */
+
     @Bean
     fun projectEsService() = eventSourcingServiceFactory.create<UUID, ProjectAggregate, ProjectAggregateState>()
 
@@ -61,20 +50,5 @@ class EventSourcingLibConfiguration {
     fun userEsService() = eventSourcingServiceFactory.create<UUID, UserAggregate, UserAggregateState>()
 
     @PostConstruct
-    fun init() {
-        // Demonstrates how to explicitly subscribe the instance of annotation based subscriber to some stream. See the [AggregateSubscriptionsManager]
-       // subscriptionsManager.subscribe<ProjectAggregate>(projectEventSubscriber)
-
-        // Demonstrates how you can set up the listeners to the event stream
-        eventStreamManager.maintenance {
-            onRecordHandledSuccessfully { streamName, eventName ->
-                logger.info("Stream $streamName successfully processed record of $eventName")
-            }
-
-            onBatchRead { streamName, batchSize ->
-                logger.info("Stream $streamName read batch size: $batchSize")
-            }
-        }
-    }
-
+    fun init() {}
 }
